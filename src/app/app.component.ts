95d42e38d070a499ee7e45f496c78239dd6503f8
Shamanoff <shamanoff@gmail.com>
2018-01-14 09:10:58 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+declare var $:any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  ngOnInit() {
+    $(document).foundation();
+  }
+}
